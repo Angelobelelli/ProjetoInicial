219be4bfdfac04b2b75625b1b10841c6b20ed4a1
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2025-02-07 08:34:34 -0300

    .

---
+++ projetoinicial/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import MyComponent from "./components/MyComponent";
+
+function App() {
+  return (
+    <>
+      <h1>Meu título</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius possimus deleniti rerum ut veritatis cupiditate aliquam necessitatibus hic. Dolor nostrum consectetur asperiores voluptatibus voluptas totam error eius maxime corrupti nihil?</p>
+      <MyComponent/>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ projetoinicial/src/components/BarraDeNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BarraDeNav = () => {
+  return (
+    <div>
+      <h2>Meu Componente, criado por mim mesmo</h2>
+      <p>autoria: Eu</p>
+      
+    </div>
+  )
+}
+
+export default BarraDeNav
